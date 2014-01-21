0780d7d14bff9787c339d80ba3f8599cfc828e66
Jeremy Mitchell <jeremy@flexdevelopers.com>
2014-01-21 13:46:19 -0700

    creates user resource

---
+++ app/controllers/users_controller.rb
@@ -0,0 +1,61 @@
+class UsersController < ApplicationController
+  before_action :set_user, only: [:show, :update, :destroy]
+
+  # GET /users
+  # GET /users.json
+  def index
+    @users = User.all
+    render json: @users
+  end
+
+  # GET /users/1
+  # GET /users/1.json
+  def show
+    render json: @user
+  end
+
+  # GET /users/new
+  def new
+    @user = User.new
+    render json: @user
+  end
+
+  # POST /users
+  # POST /users.json
+  def create
+    @user = User.new(user_params)
+    if @user.save
+      render json: @user, status: :created, location: @user
+    else
+      render json: @user.errors, status: :unprocessable_entity
+    end
+  end
+
+  # PATCH/PUT /users/1
+  # PATCH/PUT /users/1.json
+  def update
+    if @user.update_attributes(user_params)
+      head :no_content
+    else
+      render json: @user.errors, status: :unprocessable_entity
+    end
+  end
+
+  # DELETE /users/1
+  # DELETE /users/1.json
+  def destroy
+    @user.destroy
+    head :no_content
+  end
+
+  private
+    # Use callbacks to share common setup or constraints between actions.
+    def set_user
+      @user = User.find(params[:id])
+    end
+
+    # Never trust parameters from the scary internet, only allow the white list through.
+    def user_params
+      params.require(:user).permit(:email, :password)
+    end
+end
