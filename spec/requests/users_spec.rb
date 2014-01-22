92f3f2c91bfe522429288342bea62e2d93d2d879
Jeremy Mitchell <jeremy@flexdevelopers.com>
2014-01-21 20:48:20 -0700

    adds remaining user controller tests

--- spec/requests/users_spec.rb
+++ spec/requests/users_spec.rb
@@ -20,4 +20,35 @@ describe 'Users API' do
     end
   end
 
+  describe 'GET /users/new' do
+    it 'returns a new user' do
+      get new_user_path
+      expect(response).to be_success
+    end
+  end
+
+  describe 'POST /users' do
+    it 'creates a user' do
+      user_params = FactoryGirl.attributes_for(:user)
+      expect { post users_path, user: user_params }.to change(User, :count).by(1)
+    end
+  end
+
+  describe 'PATCH /users/:id' do
+    it 'updates a user' do
+      user = FactoryGirl.create(:user)
+      user.email = 'bar@foo.com'
+      patch user_path(user), user: user.attributes
+      user.reload
+      user.email.should == 'bar@foo.com'
+    end
+  end
+
+  describe 'DELETE /users/:id' do
+    it 'deletes a user' do
+      user = FactoryGirl.create(:user)
+      expect { delete user_path(user), id: user.id }.to change(User, :count).by(-1)
+    end
+  end
+
 end
