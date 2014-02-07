ba49fc48358fd610490edf59e42d8467086729d1
Jeremy Mitchell <jeremy@flexdevelopers.com>
2014-02-06 20:29:00 -0700

    adds show action to leagues controller

--- app/controllers/api/leagues_controller.rb
+++ app/controllers/api/leagues_controller.rb
@@ -1,6 +1,19 @@
 class API::LeaguesController < ApplicationController
+  before_action :set_user, only: [:show]
+
   def index
     @leagues = League.find_by(user_id: params[:user_id])
     render json: @leagues
   end
+
+  def show
+    @league = @user.leagues.find(params[:id])
+    render json: @league
+  end
+
+  private
+
+    def set_user
+      @user = User.find(params[:user_id])
+    end
 end
--- spec/requests/leagues_spec.rb
+++ spec/requests/leagues_spec.rb
@@ -1,7 +1,7 @@
 require 'spec_helper'
 
 describe 'Leagues API' do
-  describe 'GET /users/:id/leagues' do
+  describe 'GET /api/users/:user_id/leagues' do
     it 'returns a list of leagues for the specified user' do
       user = FactoryGirl.create(:user)
       FactoryGirl.create(:league, user: user)
@@ -12,6 +12,16 @@ describe 'Leagues API' do
       get api_user_leagues_path(user)
       expect(response).to be_success
       expect(json.length).to eq(5)
+    end
+  end
+
+  describe 'GET /api/users/:user_id/leagues/:id' do
+    it 'returns a league for a user' do
+      user = FactoryGirl.create(:user)
+      league = FactoryGirl.create(:league, user: user)
+      get api_user_league_path(user, league)
+      expect(response).to be_success
+      expect(json[:name]).should == user[:name]
     end
   end
 end
