20926fb7dd94201ebea6ba2f3753d7107d61c458
Jeremy Mitchell <jeremy@flexdevelopers.com>
2014-02-13 20:49:34 -0700

    builds out teams controller

--- app/controllers/api/teams_controller.rb
+++ app/controllers/api/teams_controller.rb
@@ -1,4 +1,7 @@
 class API::TeamsController < ApplicationController
+  before_action :set_user, only: [:index]
+  before_action :set_league, only: [:index, :new, :create]
+  before_action :set_team, only: [:show, :update, :destroy]
 
   # GET /api/teams
   # GET /api/teams.json
@@ -7,13 +10,20 @@ class API::TeamsController < ApplicationController
   # GET /api/leagues/:league_id/teams
   # GET /api/leagues/:league_id/teams.json
   def index
-    render json: {}
+    if @user
+      @teams = @user.teams
+    elsif @league
+      @teams = @league.teams
+    else
+      @teams = Team.all
+    end
+    render json: @teams
   end
 
   # GET /api/teams/1
   # GET /api/teams/1.json
   def show
-    render json: {}
+    render json: @team
   end
 
   # GET /api/leagues/:league_id/teams/new
@@ -25,19 +35,48 @@ class API::TeamsController < ApplicationController
   # POST /api/leagues/:league_id/teams
   # POST /api/leagues/:league_id/teams.json
   def create
-    render json: {}
+    @team = @league.teams.new(team_params)
+    if @team.save
+      render json: @team, status: :created, location: api_team_path(@team)
+    else
+      render json: @team.errors, status: :unprocessable_entity
+    end
   end
 
   # PATCH/PUT /api/teams/1
   # PATCH/PUT /api/teams/1.json
   def update
-    render json: {}
+    if @team.update_attributes(team_params)
+      head :no_content
+    else
+      render json: @team.errors, status: :unprocessable_entity
+    end
   end
 
   # DELETE /api/teams/1
   # DELETE /api/teams/1.json
   def destroy
-    render json: {}
+    @team.destroy
+    head :no_content
   end
+
+  private
+
+    def set_user
+      @user = User.find(params[:user_id]) if params[:user_id]
+    end
+
+    def set_league
+      @league = League.find(params[:league_id]) if params[:league_id]
+    end
+
+    def set_team
+      @team = Team.find(params[:id])
+    end
+
+    # Never trust parameters from the scary internet, only allow the white list through.
+    def team_params
+      params.require(:team).permit(:name)
+    end
 
 end
--- spec/requests/teams_spec.rb
+++ spec/requests/teams_spec.rb
@@ -5,7 +5,12 @@ describe API::TeamsController do
   # GET /api/teams
   describe '#index - all teams' do
     it 'returns a list of all teams' do
+      FactoryGirl.create(:team)
+      FactoryGirl.create(:team)
+      FactoryGirl.create(:team)
       get api_teams_path
+      expect(response).to be_success
+      expect(json.length).to eq(3)
     end
   end
 
@@ -13,7 +18,12 @@ describe API::TeamsController do
   describe '#index - league teams' do
     it 'returns a list of teams for the specified league' do
       league = FactoryGirl.create(:league)
+      FactoryGirl.create(:team)
+      FactoryGirl.create(:team, league: league)
+      FactoryGirl.create(:team, league: league)
       get api_league_teams_path(league)
+      expect(response).to be_success
+      expect(json.length).to eq(2)
     end
   end
 
@@ -21,7 +31,12 @@ describe API::TeamsController do
   describe '#index - user teams' do
     it 'returns a list of teams for the specified user' do
       user = FactoryGirl.create(:user)
+      FactoryGirl.create(:team)
+      FactoryGirl.create(:team, user: user)
+      FactoryGirl.create(:team, user: user)
       get api_user_teams_path(user)
+      expect(response).to be_success
+      expect(json.length).to eq(2)
     end
   end
 
@@ -30,6 +45,8 @@ describe API::TeamsController do
     it 'returns a team' do
       team = FactoryGirl.create(:team)
       get api_team_path(team)
+      expect(response).to be_success
+      expect(json['name']).should == 'Fire Breathing Rubber Duckies'
     end
   end
 
@@ -38,6 +55,7 @@ describe API::TeamsController do
     it 'returns a new team for a specified league' do
       league = FactoryGirl.create(:league)
       get new_api_league_team_path(league)
+      expect(response).to be_success
     end
   end
 
@@ -45,7 +63,7 @@ describe API::TeamsController do
   describe '#create' do
     it 'creates a team for a specified league' do
       team = FactoryGirl.create(:team)
-      post api_league_teams_path(team.league, team), team: team.attributes
+      expect { post api_league_teams_path(team.league, team), team: team.attributes }.to change(team.league.teams, :count).by(1)
     end
   end
 
@@ -53,15 +71,18 @@ describe API::TeamsController do
   describe '#update' do
     it 'updates a team' do
       team = FactoryGirl.create(:team)
+      team.name = 'Average Joes'
       patch api_team_path(team), team: team.attributes
+      team.reload
+      expect(team.name).to eq('Average Joes')
     end
   end
 
-  # DELETE /api/leagues/:id
+  # DELETE /api/teams/:id
   describe '#destroy' do
     it 'deletes a team' do
       team = FactoryGirl.create(:team)
-      delete api_team_path(team)
+      expect { delete api_team_path(team) }.to change(Team, :count).by(-1)
     end
   end
 
