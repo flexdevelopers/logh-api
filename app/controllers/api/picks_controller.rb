50fe2f74b17487580bd65ca00b083e092cb4d7f5
Jeremy Mitchell <jeremy@flexdevelopers.com>
2014-02-16 12:49:20 -0700

    adds the pick model

---
+++ app/controllers/api/picks_controller.rb
@@ -0,0 +1,2 @@
+class API::PicksController < ApplicationController
+end
---
+++ spec/models/pick_spec.rb
@@ -0,0 +1,6 @@
+require 'spec_helper'
+
+describe Pick do
+  it { should respond_to(:created_at) }
+  it { should respond_to(:updated_at) }
+end
