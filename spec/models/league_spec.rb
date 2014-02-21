37440b33ff2b3a2f72594b529f329411413dbb81
Jeremy Mitchell <jeremy@flexdevelopers.com>
2014-02-21 15:00:24 -0700

    adds expectations for league model

--- spec/models/league_spec.rb
+++ spec/models/league_spec.rb
@@ -1,9 +1,20 @@
 require 'spec_helper'
 
 describe League do
+
   it { should respond_to(:name) }
+  its(:name) { should be_blank }
+
   it { should respond_to(:password) }
+  its(:password) { should be_blank }
+
   it { should respond_to(:max_teams_per_user) }
+  its(:max_teams_per_user) { should be_zero }
+
   it { should respond_to(:user) }
+  its(:user) { should be_nil }
+
   it { should respond_to(:teams) }
+  its(:teams) { should be_empty }
+
 end
