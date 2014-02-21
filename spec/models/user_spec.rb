81a73432487a555a9e0a9ea2cebda132c19c8a70
Jeremy Mitchell <jeremy@flexdevelopers.com>
2014-02-21 15:05:34 -0700

    adds expectations to user model spec

--- spec/models/user_spec.rb
+++ spec/models/user_spec.rb
@@ -1,42 +1,54 @@
 require 'spec_helper'
 
 describe User do
-  it { should respond_to(:first_name) }
-  it { should respond_to(:last_name) }
-  it { should respond_to(:email) }
-  it { should respond_to(:password) }
-  it { should respond_to(:leagues) }
-  it { should respond_to(:teams) }
 
-  describe 'when first name is not present' do
+  it { should respond_to(:first_name) }
+  its(:first_name) { should be_blank }
+
+  it { should respond_to(:last_name) }
+  its(:last_name) { should be_blank }
+
+  it { should respond_to(:email) }
+  its(:email) { should be_blank }
+
+  it { should respond_to(:password) }
+  its(:password) { should be_blank }
+
+  it { should respond_to(:leagues) }
+  its(:leagues) { should be_empty }
+
+  it { should respond_to(:teams) }
+  its(:teams) { should be_empty }
+
+  context 'when first name is not present' do
     it 'should be invalid' do
       user = FactoryGirl.build(:user, first_name: '')
       expect(user).not_to be_valid
     end
   end
 
-  describe 'when first name is longer than 20 characters' do
+  context 'when first name is longer than 20 characters' do
     it 'should be invalid' do
       user = FactoryGirl.build(:user, first_name: 'a' * 21)
       expect(user).not_to be_valid
     end
   end
 
-  describe 'when last name is not present' do
+  context 'when last name is not present' do
     it 'should be invalid' do
       user = FactoryGirl.build(:user, last_name: '')
       expect(user).not_to be_valid
     end
   end
 
-  describe 'when last name is longer than 20 characters' do
+  context 'when last name is longer than 20 characters' do
     it 'should be invalid' do
       user = FactoryGirl.build(:user, last_name: 'z' * 21)
       expect(user).not_to be_valid
     end
   end
 
-  describe 'when email address is not unique' do
+  context 'when email address is not unique' do
     it 'should be invalid' do
       user1 = FactoryGirl.create(:user)
       user2 = FactoryGirl.build(:user, email: user1.email.upcase)
@@ -44,7 +56,7 @@ describe User do
     end
   end
 
-  describe 'when email format is invalid' do
+  context 'when email format is invalid' do
     it 'should be invalid' do
       addresses = %w{user@foo,com user@foo...com user_at_foo.org example.user@foo. foo@bar_baz.com foo@bar+baz.com}
       addresses.each do |invalid_address|
@@ -54,7 +66,7 @@ describe User do
     end
   end
 
-  describe 'when email format is valid' do
+  context 'when email format is valid' do
     it 'should be valid' do
       addresses = %w{user@foo.COM A_US-ER@f.b.org frst.lst@foo.jp a+b@baz.cn}
       addresses.each do |valid_address|
@@ -64,7 +76,7 @@ describe User do
     end
   end
 
-  describe 'when email has upper case characters' do
+  context 'when email has upper case characters' do
     let(:mixed_case_email) { 'Foo@Bar.com' }
 
     it 'should be saved as lowercase' do
@@ -73,10 +85,11 @@ describe User do
     end
   end
 
-  describe 'when password is less than 6 characters' do
+  context 'when password is less than 6 characters' do
     it 'should be invalid' do
       user = FactoryGirl.build(:user, password: 'fooba')
       expect(user).not_to be_valid
     end
   end
+
 end
