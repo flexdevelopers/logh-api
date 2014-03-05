7be6d3ff40854cfe97463959e0400c614523b8b6
Jeremy Mitchell <jeremy@flexdevelopers.com>
2014-03-04 22:12:00 -0700

    fixes presence validation on team.alive (boolean value)

--- app/models/team.rb
+++ app/models/team.rb
@@ -6,5 +6,5 @@ class Team < ActiveRecord::Base
   validates :name, presence: true, length: { maximum: 30 }
   validates :league, presence: true
   validates :user, presence: true
-  validates :alive, presence: true
+  validates :alive, inclusion: { in: [true, false] }
 end
