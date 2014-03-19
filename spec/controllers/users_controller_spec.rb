0879c7e34a96e4e21f11753743204328f745739e
Jeremy Mitchell <jeremy@flexdevelopers.com>
2014-03-18 21:38:06 -0600

    fixes user controller specs to work within the context of the current user

--- spec/controllers/users_controller_spec.rb
+++ spec/controllers/users_controller_spec.rb
@@ -1,16 +1,16 @@
   require 'spec_helper'
 
 describe API::UsersController do
+  let(:user) { FactoryGirl.create(:user) }
 
   before do
     request_with_api_token
-    sign_in(FactoryGirl.create(:user))
+    sign_in(user)
   end
 
   # GET /api/users/:id
   describe '#show' do
     it 'returns a user' do
-      user = FactoryGirl.create(:user)
       get :show, id: user.id
       expect(response).to be_success
       expect(json['email']).to eq(user.email)
@@ -28,9 +28,8 @@ describe API::UsersController do
 
   # PATCH/PUT /api/users/:id
   describe '#update' do
+    before { user.email = 'bar@foo.com' }
     it 'updates a user' do
-      user = FactoryGirl.create(:user)
-      user.email = 'bar@foo.com'
       patch :update, id: user.id, user: user.attributes
       user.reload
       user.email.should == 'bar@foo.com'
