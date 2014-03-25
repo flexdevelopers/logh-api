007d11702b49b8c409bf9ed8199b0700611d85ad
Jeremy Mitchell <jeremy@flexdevelopers.com>
2014-03-24 18:02:12 -0600

    limits users#show and users#update to the current authenticated user

--- app/controllers/api/users_controller.rb
+++ app/controllers/api/users_controller.rb
@@ -1,14 +1,13 @@
 class API::UsersController < API::BaseController
   skip_before_filter :authenticate, only: [:create]
+  before_action :_verify_user, only: [:show, :update]
 
   # GET /api/users/1
-  # GET /api/users/1.json
   def show
     render json: current_user
   end
 
   # POST /api/users
-  # POST /api/users.json
   def create
     user = User.new(_user_params)
     if user.save
@@ -19,9 +18,8 @@ class API::UsersController < API::BaseController
   end
 
   # PATCH/PUT /api/users/1
-  # PATCH/PUT /api/users/1.json
   def update
-    if current_user.update_attributes(_user_params)
+    if current_user.update(_user_params)
       head :no_content
     else
       render json: current_user.errors, status: :unprocessable_entity
@@ -29,6 +27,10 @@ class API::UsersController < API::BaseController
   end
 
   private
+
+    def _verify_user
+      not_authorized() unless params[:id].to_i == current_user.id
+    end
 
     def _user_params
       params.require(:user).permit(:first_name, :last_name, :email, :password, :password_confirmation)
--- spec/controllers/users_controller_spec.rb
+++ spec/controllers/users_controller_spec.rb
@@ -1,18 +1,27 @@
   require 'spec_helper'
 
 describe API::UsersController do
-  let(:user) { FactoryGirl.create(:user) }
+  let(:current_user) { FactoryGirl.create(:user, email: 'foo@bar.com') }
+  let(:another_user) { FactoryGirl.create(:user, email: 'foo@baz.com') }
 
   before do
-    sign_in(user)
+    sign_in(current_user)
   end
 
   # GET /api/users/:id
   describe '#show' do
-    it 'returns a user' do
-      get :show, id: user.id
-      expect(response).to be_success
-      expect(json['email']).to eq(user.email)
+    context 'when requesting the signed in user' do
+      it 'returns a user' do
+        get :show, id: current_user.id
+        expect(response).to be_success
+        expect(json['email']).to eq(current_user.email)
+      end
+    end
+    context 'when requesting a user other than the signed in user' do
+      it 'returns unauthorized' do
+        get :show, id: another_user.id
+        expect(response.status).to eq(401)
+      end
     end
   end
 
@@ -27,12 +36,25 @@ describe API::UsersController do
 
   # PATCH/PUT /api/users/:id
   describe '#update' do
-    before { user.email = 'bar@foo.com' }
-    it 'updates a user' do
-      patch :update, id: user.id, user: user.attributes
-      user.reload
-      user.email.should == 'bar@foo.com'
+    context 'when attempting to update the signed in user' do
+      before { current_user.email = 'newemail@foo.com' }
+      it 'updates the user' do
+        patch :update, id: current_user.id, user: current_user.attributes
+        expect(response).to be_success
+        current_user.reload
+        current_user.email.should == 'newemail@foo.com'
+      end
     end
+    context 'when attempting to update a user other than the signed in user' do
+      before { another_user.email = 'newemail@foo.com' }
+      it 'returns unauthorized and does not update the user' do
+        patch :update, id: another_user.id, user: another_user.attributes
+        expect(response.status).to eq(401)
+        another_user.reload
+        another_user.email.should == 'foo@baz.com'
+      end
+    end
+
   end
 
 end
