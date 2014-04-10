b5fc91bd19122b34d9df84a27546d78886bfe5da
Jeremy Mitchell <jeremy@flexdevelopers.com>
2014-04-10 16:34:48 -0600

    sets up scaffold for angular app

---
+++ angular/app/scripts/app.js
@@ -0,0 +1,20 @@
+'use strict';
+
+angular
+  .module('angularApp', [
+    'ui.bootstrap',
+    'ngCookies',
+    'ngResource',
+    'ngSanitize',
+    'ngRoute'
+  ])
+  .config(function ($routeProvider) {
+    $routeProvider
+      .when('/', {
+        templateUrl: 'views/main.html',
+        controller: 'MainCtrl'
+      })
+      .otherwise({
+        redirectTo: '/'
+      });
+  });
---
+++ angular/app/scripts/controllers/main.js
@@ -0,0 +1,10 @@
+'use strict';
+
+angular.module('angularApp')
+  .controller('MainCtrl', function ($scope) {
+    $scope.awesomeThings = [
+      'footon',
+      'tom',
+      'cars'
+    ];
+  });
