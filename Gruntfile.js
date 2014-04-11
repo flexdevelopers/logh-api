6b43eaf760e306c314f893d89594e885c653a7d9
Jeremy Mitchell <jeremy@flexdevelopers.com>
2014-04-10 18:20:01 -0600

    adds a grunt-hub so angular/gruntfile.js can run on heroku deploy

---
+++ Gruntfile.js
@@ -0,0 +1,21 @@
+/**
+ * Example Grunt Hub
+ *
+ * Edit the hub.all.src to point to your Gruntfile locations.
+ * Then run `grunt`.
+ */
+module.exports = function(grunt) {
+  'use strict';
+
+  grunt.initConfig({
+    hub: {
+      all: {
+        src: ['angular/Gruntfile.js'],
+      },
+    },
+  });
+
+  grunt.loadNpmTasks('grunt-hub');
+
+  grunt.registerTask('default', ['hub']);
+};
