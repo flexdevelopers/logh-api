567c01ea83b8761f65db04092c7d2fda52ae05bd
Jeremy Mitchell <jeremy@flexdevelopers.com>
2014-04-14 12:04:53 -0600

    test push

--- angular/app/scripts/controllers/main.js
+++ angular/app/scripts/controllers/main.js
@@ -1,10 +1,13 @@
 'use strict';
 
 angular.module('angularApp')
-  .controller('MainCtrl', function ($scope) {
-    $scope.awesomeThings = [
-      'footon',
-      'tom',
-      'goodman'
-    ];
+  .controller('MainCtrl', function ($http) {
+    var app = this;
+    $http({method: 'GET', url: '/api/seasons/2/leagues'}).
+      success(function(data, status, headers, config) {
+        app.leagues = data;
+      }).
+      error(function(data, status, headers, config) {
+        console.log('no luck');
+      });
   });
