9ee7cfd90264786039e8a53a8dde278e66345328
Jeremy Mitchell <mitchell852@gmail.com>
2014-04-29 10:13:54 -0600

    spec changes to account for user being returned from login and create user

--- spec/controllers/users_controller_spec.rb
+++ spec/controllers/users_controller_spec.rb
@@ -25,7 +25,8 @@ describe API::UsersController do
       user_params = FactoryGirl.attributes_for(:user)
       expect { post :create, user: user_params }.to change(User, :count).by(1)
       expect(response).to be_success
-      expect(json[:token]).not_to be_blank
+      expect(json[:payload][:user]).not_to be_blank
+      expect(json[:payload][:token]).not_to be_blank
     end
     it 'sends a welcome email' do
       user = FactoryGirl.create(:user)
