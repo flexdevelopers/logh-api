362caf51489c9395fcbe1481f8291dbafce1cf8c
Jeremy Mitchell <mitchell852@gmail.com>
2014-05-01 15:34:28 -0600

    nests views for league states

--- client/app/src/modules/league/index.js
+++ client/app/src/modules/league/index.js
@@ -3,13 +3,9 @@ module.exports = angular.module('loghApp.league', [])
     .controller('LeagueController', require('./LeagueController'))
     .config(function($stateProvider, $urlRouterProvider) {
         $stateProvider
-            .state('league', {
+            .state('app.league', {
                 abstract: true,
-                url: '/seasons/:seasonId/leagues',
-                templateUrl: 'modules/league/league.tpl.html'
-            })
-            .state('league.list', {
-                url: '',
+                url: 'seasons/:seasonId/leagues',
                 views: {
                     header: {
                         templateUrl: 'common/modules/header/header.tpl.html',
@@ -20,31 +16,33 @@ module.exports = angular.module('loghApp.league', [])
                         controller: 'MessageController'
                     },
                     content: {
-                        templateUrl: 'modules/league/league.list.tpl.html',
-                        controller: 'LeagueController'
+                        templateUrl: 'modules/league/league.tpl.html'
                     },
                     footer: {
                         templateUrl: 'common/templates/footer.tpl.html'
                     }
                 }
             })
-            .state('league.new', {
+            .state('app.league.list', {
+                url: '',
+                views: {
+                    leagueContent: {
+                        templateUrl: 'modules/league/league.list.tpl.html',
+                        controller: 'LeagueController'
+                    }
+                },
+                resolve: {
+                    weeks: function() {
+                        return [];
+                    }
+                }
+            })
+            .state('app.league.new', {
                 url: '/new',
                 views: {
-                    header: {
-                        templateUrl: 'common/modules/header/header.tpl.html',
-                        controller: 'HeaderController'
-                    },
-                    message: {
-                        templateUrl: 'common/modules/message/message.tpl.html',
-                        controller: 'MessageController'
-                    },
-                    content: {
+                    leagueContent: {
                         templateUrl: 'modules/league/league.new.tpl.html',
                         controller: 'LeagueController'
-                    },
-                    footer: {
-                        templateUrl: 'common/templates/footer.tpl.html'
                     }
                 },
                 resolve: {
@@ -56,23 +54,17 @@ module.exports = angular.module('loghApp.league', [])
                     }
                 }
             })
-            .state('league.detail', {
+            .state('app.league.detail', {
                 url: '/:leagueId',
                 views: {
-                    header: {
-                        templateUrl: 'common/modules/header/header.tpl.html',
-                        controller: 'HeaderController'
-                    },
-                    message: {
-                        templateUrl: 'common/modules/message/message.tpl.html',
-                        controller: 'MessageController'
-                    },
-                    content: {
+                    leagueContent: {
                         templateUrl: 'modules/league/league.detail.tpl.html',
                         controller: 'LeagueController'
-                    },
-                    footer: {
-                        templateUrl: 'common/templates/footer.tpl.html'
+                    }
+                },
+                resolve: {
+                    weeks: function() {
+                        return [];
                     }
                 }
             })
