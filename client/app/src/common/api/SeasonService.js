5a7809e2ca623bacb9562c349d78a300adf83c16
Jeremy Mitchell <mitchell852@gmail.com>
2014-05-01 15:00:50 -0600

    typo

--- client/app/src/common/api/SeasonService.js
+++ client/app/src/common/api/SeasonService.js
@@ -4,7 +4,7 @@ var SeasonService = function($http, $log, apiConfig) {
         $log.log('SeasonService: get current season');
         var promise = $http.get(apiConfig.baseURL + "seasons/current")
             .success(function(data) {
-                $log.log("UserService: get current season success");
+                $log.log("SeasonService: get current season success");
                 return data;
             })
             .error(function(data) {
