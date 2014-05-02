19463b9f71d25dff9bdc7928ca05f3d0aa07afce
Jeremy Mitchell <mitchell852@gmail.com>
2014-05-02 15:31:46 -0600

    validates create league form

--- client/app/src/modules/league/LeagueController.js
+++ client/app/src/modules/league/LeagueController.js
@@ -3,17 +3,17 @@ var LeagueController = function($scope, $log, $stateParams, weeks) {
 
     $scope.isPasswordHidden = true;
 
+    $scope.weeks = weeks.data;
+
     $scope.newLeagueData = {
         name: '',
         season_id: $stateParams.seasonId,
-        start_week_id: '',
+        start_week_id: $scope.weeks[0].id,
         public: true,
         max_teams_per_user: '',
         password: '',
         password_confirmation: ''
     };
-
-    $scope.weeks = weeks.data;
 
     $scope.hidePasswordFields = function(value) {
         $scope.isPasswordHidden = value;
