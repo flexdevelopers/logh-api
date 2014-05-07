54daa0ec116b3149c26428f91c8ca33f207ee1ce
Jeremy Mitchell <mitchell852@gmail.com>
2014-05-07 10:47:51 -0600

    builds out layout of app.leagues page

--- client/app/src/modules/league/LeaguesController.js
+++ client/app/src/modules/league/LeaguesController.js
@@ -3,6 +3,21 @@ var LeaguesController = function($scope, $log, leagues) {
 
     $scope.leagues = leagues.data.payload.leagues;
 
+    $scope.totalItems = 64;
+    $scope.currentPage = 4;
+
+    $scope.setPage = function (pageNo) {
+        $scope.currentPage = pageNo;
+    };
+
+    $scope.pageChanged = function() {
+        console.log('Page changed to: ' + $scope.currentPage);
+    };
+
+    $scope.maxSize = 5;
+    $scope.bigTotalItems = 175;
+    $scope.bigCurrentPage = 1;
+
     /**
      * Invoked on startup, like a constructor.
      */
