bd1a9b2c88896bde0a63d2a37e46dbbb3ba96446
Jeremy Mitchell <mitchell852@gmail.com>
2014-05-09 09:50:21 -0600

    renames focus directive

--- client/app/src/common/directives/focus/index.js
+++ client/app/src/common/directives/focus/index.js
@@ -1,4 +1,4 @@
 //application directives
 module.exports = angular.module('loghApp.directives.focus',[])
-    .directive('focus', require('./FocusDirective'))
+    .directive('loghFocus', require('./FocusDirective'))
 ;
