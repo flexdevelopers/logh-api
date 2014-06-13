7faa679add3c9696694192be0df13d214534d1df
Jeremy Mitchell <mitchell852@gmail.com>
2014-06-12 21:31:58 -0600

    must explicitly set the location if you want 401 redirect to occur

--- client/app/src/modules/public/home/HomeController.js
+++ client/app/src/modules/public/home/HomeController.js
@@ -1,16 +1,24 @@
 
-var HomeController = function($scope, $log, seasonModel) {
+var HomeController = function($scope, $log, $location, seasonModel) {
 
     $scope.season = seasonModel.season;
+
+    $scope.createLeague = function(season) {
+        $location.path('season/' + season.id + '/league/new');
+    };
+
+    $scope.createTeam = function(season) {
+        $location.path('season/' + season.id + '/leagues/public');
+    };
 
     /**
      * Invoked on startup, like a constructor.
      */
     var init = function () {
-        $log.debug("main controller");
+        $log.debug("home controller");
     };
     init();
 };
 
-HomeController.$inject = ['$scope', '$log', 'seasonModel'];
+HomeController.$inject = ['$scope', '$log', '$location', 'seasonModel'];
 module.exports = HomeController;
