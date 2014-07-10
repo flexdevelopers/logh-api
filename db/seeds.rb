7216e8289473522a68a934631f116fd5fcb8a38c
Jeremy Mitchell <mitchell852@gmail.com>
2014-07-10 07:29:27 -0600

    updates seed data

--- db/seeds.rb
+++ db/seeds.rb
@@ -42,7 +42,7 @@ season = Season.create!(name: '2014-15 NFL Season')
 
 # create 5 weeks for the season
 5.times do |i|
-  week = Week.create!(number: i + 1, week_type: WeekType.find_by(code: 'reg'), starts_at: (i - 4).weeks.from_now, season: season)
+  week = Week.create!(number: i + 1, week_type: WeekType.find_by(code: 'reg'), starts_at: (i - 4).weeks.from_now.to_date, season: season)
   # create 3 games for each week
   3.times do |i|
     game = Game.create!(starts_at: week.starts_at + 1.day, week: week, home_squad: home_squads[i], visiting_squad: visiting_squads[i] )
