41ed804fb9257925a057bd57a2d07d11f8440069
Jeremy Mitchell <mitchell852@gmail.com>
2014-07-21 10:40:12 -0600

    more changes to the message boards

--- client/app/src/modules/private/team/view/ViewTeamController.js
+++ client/app/src/modules/private/team/view/ViewTeamController.js
@@ -6,7 +6,7 @@ var ViewTeamController = function(team, regularPicks, playoffPicks, $scope, $log
   $scope.playoffPicks = playoffPicks.data;
 
   $scope.message = function(team) {
-    var message = "This team's message board is empty";
+    var message = "Zip. Zilch. Nada";
     if (team.message && team.message.length > 0) {
       message = team.message;
     }
