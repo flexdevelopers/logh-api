a69de093f8680b9e561effcc20bb97f5e6e45874
Jeremy Mitchell <mitchell852@gmail.com>
2014-08-03 10:39:50 -0600

    gets rid of register state

--- client/app/src/modules/public/register/index.js
+++ client/app/src/modules/public/register/index.js
@@ -1,27 +1,2 @@
-
-module.exports = angular.module('loghApp.public.register', [])
-    .controller('RegisterController', require('./RegisterController'))
-    .config(function($stateProvider, $urlRouterProvider) {
-        $stateProvider
-            .state('public.register', {
-                url: 'register',
-                views: {
-                    header: {
-                        templateUrl: 'common/modules/header/header.tpl.html',
-                        controller: 'HeaderController'
-                    },
-                    message: {
-                        templateUrl: 'common/modules/message/message.tpl.html',
-                        controller: 'MessageController'
-                    },
-                    content: {
-                        templateUrl: 'modules/public/register/register.tpl.html',
-                        controller: 'RegisterController'
-                    },
-                    footer: {
-                        templateUrl: 'common/templates/footer.tpl.html'
-                    }
-                }
-            });
-        $urlRouterProvider.otherwise('/');
-    });
+module.exports = angular.module('loghApp.register', [])
+  .controller('RegisterController', require('./RegisterController'));
