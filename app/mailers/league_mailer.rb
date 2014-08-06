c00d193eff26d9b778259d82bf8aa312de75eca8
Jeremy Mitchell <mitchell852@gmail.com>
2014-08-05 20:40:03 -0600

    gets rid of notification when start week changes

--- app/mailers/league_mailer.rb
+++ app/mailers/league_mailer.rb
@@ -20,16 +20,4 @@ class LeagueMailer < ActionMailer::Base
     mail to: 'jeremy@loseorgohome.com', subject: "The #{@league.name} league has a new message from the commish"
   end
 
-  def start_week_change(league)
-    @league = league
-
-    hdr = SmtpApiHeader.new
-    receiver = @league.coach_emails
-    hdr.addTo(receiver)
-
-    headers['X-SMTPAPI'] =  hdr.asJSON()
-
-    mail to: 'jeremy@loseorgohome.com', subject: "The start week for the #{@league.name} league has changed"
-  end
-
 end
