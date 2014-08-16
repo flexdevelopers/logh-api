2727e4b289a5f565ccd969200dba6a880d758811
Jeremy Mitchell <mitchell852@gmail.com>
2014-08-15 21:41:36 -0600

    weeks.starts_at needs to be a datetime

---
+++ db/migrate/20140816033318_change_starts_at_to_datetime.rb
@@ -0,0 +1,8 @@
+class ChangeStartsAtToDatetime < ActiveRecord::Migration
+  def up
+    change_column :weeks, :starts_at, :datetime, null: false
+  end
+  def down
+    change_column :weeks, :starts_at, :date, null: false
+  end
+end
--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20140809023453) do
+ActiveRecord::Schema.define(version: 20140816033318) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -166,7 +166,7 @@ ActiveRecord::Schema.define(version: 20140809023453) do
 
   create_table "weeks", force: true do |t|
     t.integer  "number",                       null: false
-    t.date     "starts_at",                    null: false
+    t.datetime "starts_at",                    null: false
     t.datetime "created_at"
     t.datetime "updated_at"
     t.integer  "season_id",                    null: false
