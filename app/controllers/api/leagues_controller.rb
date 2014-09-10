005f7994ca7df5ab45859a01315d9573b7ac23fa
Jeremy Mitchell <mitchell852@gmail.com>
2014-09-09 18:37:34 -0600

    can create a league for a week that is not complete

--- app/controllers/api/leagues_controller.rb
+++ app/controllers/api/leagues_controller.rb
@@ -135,7 +135,7 @@ class API::LeaguesController < API::BaseController
 
     def _verify_start_week
       start_week = Week.find(_league_params[:start_week_id])
-      forbidden('Start week is invalid') if start_week.started?
+      forbidden('Start week is invalid') if start_week.complete
     end
 
     def _is_commish_of(league)
