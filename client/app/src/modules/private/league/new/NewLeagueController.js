3f33ea8687493992f6404bf715f84d86c467b6b2
Jeremy Mitchell <mitchell852@gmail.com>
2015-01-29 22:02:38 -0700

    adds max picks per week drop down to league create page

--- client/app/src/modules/private/league/new/NewLeagueController.js
+++ client/app/src/modules/private/league/new/NewLeagueController.js
@@ -4,6 +4,16 @@ var NewLeagueController = function(seasonId, weeks, $scope, $log, $modalInstance
 
     $scope.seasons = angular.copy(seasonModel.currentSeasons);
 
+    $scope.maxPicksPerWeek = [
+      { number: 0, display: 'Pick all games' },
+      { number: 25, display: 'Pick 25 games' },
+      { number: 15, display: 'Pick 15 games' },
+      { number: 10, display: 'Pick 10 games' },
+      { number: 5, display: 'Pick 5 games' },
+      { number: 2, display: 'Pick 2 games' },
+      { number: 1, display: 'Pick 1 game (no duplicates)' }
+    ];
+
     $scope.newLeagueData = {
         name: '',
         nickname: '',
@@ -12,7 +22,7 @@ var NewLeagueController = function(seasonId, weeks, $scope, $log, $modalInstance
         public: true,
         elimination: true,
         max_teams_per_user: '',
-        max_picks_per_week: 1
+        max_picks_per_week: $scope.maxPicksPerWeek[0].number
     };
 
     $scope.getSeasonWeeks = function() {
