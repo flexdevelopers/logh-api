23104cb9b6b90ebfefd67f161dbd8bc8b70ef162
Jeremy Mitchell <mitchell852@gmail.com>
2015-03-12 21:21:07 -0600

    adds incorrect scope to pick

--- app/models/pick.rb
+++ app/models/pick.rb
@@ -14,6 +14,7 @@ class Pick < ActiveRecord::Base
   default_scope { includes(:week).order('weeks.starts_at ASC') }
 
   scope :correct, -> { where(correct: true) }
+  scope :incorrect, -> { where(correct: false) }
   scope :not_locked, -> { joins(:game).where('games.starts_at > ?', Time.zone.now) }
 
   scope :not_none, -> { joins(:squad).where('squads.none = ?', false) }
