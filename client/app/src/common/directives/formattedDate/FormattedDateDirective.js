6b2e9ea01be4282aecb719169288979585b1a4ef
Jeremy Mitchell <mitchell852@gmail.com>
2015-05-21 20:07:47 -0600

    changes date format of calendar widget display to ddd m/d/yy h:MM tt (Z)

--- client/app/src/common/directives/formattedDate/FormattedDateDirective.js
+++ client/app/src/common/directives/formattedDate/FormattedDateDirective.js
@@ -2,11 +2,11 @@ var FormattedDateDirective = function(dateUtils) {
   return {
     link: function (scope, element, attrs, ctrl) {
       ctrl.$formatters.unshift(function (modelValue) {
-        return dateUtils.dateFormat(modelValue, "mmm d yyyy h:MM tt (Z)")
+        return dateUtils.dateFormat(modelValue, "ddd m/d/yy h:MM tt (Z)")
       });
 
       ctrl.$parsers.unshift(function (viewValue) {
-        return dateUtils.dateFormat(modelValue, "mmm d yyyy h:MM tt (Z)")
+        return dateUtils.dateFormat(modelValue, "ddd m/d/yy h:MM tt (Z)")
       });
     },
     restrict: 'A',
