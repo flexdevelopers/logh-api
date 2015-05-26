6af67f6e9e8d90c80c147914af877e6549b54074
Jeremy Mitchell <mitchell852@gmail.com>
2015-05-25 20:59:48 -0600

    no message on home page

--- client/app/src/modules/public/home/index.js
+++ client/app/src/modules/public/home/index.js
@@ -10,10 +10,6 @@ module.exports = angular.module('loghApp.public.home', [])
                         templateUrl: 'common/modules/header/header.tpl.html',
                         controller: 'HeaderController'
                     },
-                    message: {
-                        templateUrl: 'common/modules/message/message.tpl.html',
-                        controller: 'MessageController'
-                    },
                     landing: {
                         templateUrl: 'modules/public/home/home.tpl.html',
                         controller: 'HomeController'
