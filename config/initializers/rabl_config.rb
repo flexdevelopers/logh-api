30efb9968082c577a4e9376224741cb0fac61bb5
Jeremy Mitchell <mitchell852@gmail.com>
2015-06-16 18:58:26 -0600

    reverts rabl template caching

--- config/initializers/rabl_config.rb
+++ config/initializers/rabl_config.rb
@@ -1,6 +1,6 @@
 Rabl.configure do |config|
   config.include_json_root = false
-  config.cache_all_output = true
-  config.cache_sources = Rails.env.to_s != 'development' # Defaults to false
+  # config.cache_all_output = true
+  # config.cache_sources = Rails.env.to_s != 'development' # Defaults to false
   config.view_paths = [Rails.root.join("app/views")]
 end
