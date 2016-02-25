79480d23e8e80378310efdcc4a9a6082d153dc5c
Jeremy Mitchell <mitchell852@gmail.com>
2016-02-25 11:34:37 -0700

    changes access control origin

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -6,7 +6,7 @@ class ApplicationController < ActionController::Base
   before_filter :cors_set_access_control_headers
 
   def cors_set_access_control_headers
-    headers['Access-Control-Allow-Origin'] = 'https://www.loseorgohome.com'
+    headers['Access-Control-Allow-Origin'] = '*'
     headers['Access-Control-Allow-Methods'] = 'POST, GET, PUT, DELETE, OPTIONS'
     headers['Access-Control-Allow-Headers'] = '*, X-Requested-With, X-Prototype-Version, X-CSRF-Token, Content-Type'
     headers['Access-Control-Max-Age'] = "1728000"
